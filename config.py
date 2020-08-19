685e9dab05fad2d69add0c29c2bec5f22b709ef3
Mariana Mendes <mariana.silva@ccc.ufcg.edu.br>
2020-08-18 21:34:49 -0300

    config

--- config.py
+++ config.py
@@ -16,17 +16,17 @@ with open('config.json', 'r+') as f:
   ## Config in case of the user want just one consult a especific period
   ## First config for json in terminal 
 
-  specificPeriod = raw_input('1. Collect metric in a specific time window \n 2. Automatically collect metrics starting now\n')
+  specificPeriod = input('1. Collect metric in a specific time window \n 2. Automatically collect metrics starting now\n')
 
   if(specificPeriod == '1'): 
-    ans = raw_input(cons.ASK_FOR_CONFIG)
+    ans = input(cons.ASK_FOR_CONFIG)
 
     if(ans == "Y"): 
       ### perguntar se quer pra instancias especificas ou todas
       # instances = map(str,raw_input("Enter field " + cons.IDS_KEY + ": ").split())
       config_fields = [cons.METRIC_KEY, cons.START_TIME_KEY, cons.END_TIME_KEY, cons.PERIOD_KEY]
       for field in config_fields:
-        value = raw_input("Enter field " + field + ": ")
+        value = input("Enter field " + field + ": ")
         data[field] = value
 
     f.seek(0)        
@@ -37,8 +37,8 @@ with open('config.json', 'r+') as f:
     print(json.dumps(data, indent=2, sort_keys=True))
 
   else: 
-    period = raw_input('Specify the collect period (60s, 300s,...)')
-    metrics = map(str,raw_input('Which metrics do you want? (CPUUtilization, mem_usage)').split())
+    period = input('Specify the collect period (60s, 300s,...)')
+    metrics = map(str,input('Which metrics do you want? (CPUUtilization, mem_usage)').split())
 
     data['period'] = period
     data['metricName'] = metrics
