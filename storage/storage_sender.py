044de8063dce13109c8a0f54e6d9ae28b4a417ec
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2020-09-01 21:58:19 -0300

    bucket checking refactored and storage class added

--- storage/storage_sender.py
+++ storage/storage_sender.py
@@ -1,19 +1,26 @@
+import logging
 import boto3
+from botocore.exceptions import ClientError
 
 class Uploader:
     def __init__(self, aws_region):
         self.client = boto3.client('s3', region_name=aws_region)
         self.buckets = self.client.list_buckets()['Buckets']
 
-    # TODO: mandar comprimido para S3 Glacier Deep Archive? Limitar os arquivos a varrer?
-    def send2storage(self, folder_source, bucket, bucket_folder='', storage_class='STANDARD_IA':
-        if not os.path.exists(folder_source):
-            print('Error while trying to find the folder file source:', folder_source)
-            return
-        
+    def alreadyExist(bucket):
         if not bucket in [dic['Name'] for dic in self.buckets]:
             print('There is no bucket named {bucket_name} in this account'.format(bucket_name=bucket))
-            return
+            return True
+        return False
+
+    # TODO: mandar comprimido para S3 Glacier Deep Archive? Limitar os arquivos a varrer?
+    def send2storage(self, folder_source, bucket, bucket_folder='', storage_class='STANDARD_IA'):
+        if not os.path.exists(folder_source):
+            print('Error while trying to find the folder file source:', folder_source)
+            return False
+        
+        if not alreadyExist(bucket):
+            return False
         
         for file_name in os.listdir(folder_source):
             # defining the S3 object name 
