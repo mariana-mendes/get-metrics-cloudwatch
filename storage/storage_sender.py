eee28f028c53e053b984d83aaec9ce6cb96aa456
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2020-09-01 21:18:34 -0300

    send files to S3 funtion defined

---
+++ storage/storage_sender.py
@@ -0,0 +1,30 @@
+import boto3
+
+class Uploader:
+    def __init__(self, aws_region):
+        self.client = boto3.client('s3', region_name=aws_region)
+        self.buckets = self.client.list_buckets()['Buckets']
+
+    # TODO: mandar comprimido para S3 Glacier Deep Archive? Limitar os arquivos a varrer?
+    def send2storage(self, folder_source, bucket, bucket_folder='', storage_class='STANDARD_IA':
+        if not os.path.exists(folder_source):
+            print('Error while trying to find the folder file source:', folder_source)
+            return
+        
+        if not bucket in [dic['Name'] for dic in self.buckets]:
+            print('There is no bucket named {bucket_name} in this account'.format(bucket_name=bucket))
+            return
+        
+        for file_name in os.listdir(folder_source):
+            # defining the S3 object name 
+            if bucket_folder:
+                object_name='{folder_name}/{file_name}'.format(folder_name=bucket_folder, file_name=file_name)
+            else:
+                object_name=file_name
+
+            try:
+                self.client.upload_file(file_name, bucket, object_name, ExtraArgs = {'StorageClass': storage_class})
+            except ClientError as e:
+                logging.error(e)
+                return False
+        return True
