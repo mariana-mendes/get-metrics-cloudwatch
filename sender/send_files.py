28aea0eaa25dfa21a77a369a4327fb5b44218cd3
Mariana Mendes <mariana.silva@ccc.ufcg.edu.br>
2020-09-07 21:10:18 -0300

    compress files

--- sender/send_files.py
+++ sender/send_files.py
@@ -10,29 +10,32 @@ class Sender:
     def __init__(self):
         self.logger = setup_log()
         self.client = boto3.client('s3')
-        
+
     def compress_files(self):
         dirData = os.getcwd() + "/data/"
         for folder in os.listdir(dirData):
-            for fileName in os.listdir(dirData + folder):
-                print(dirData + folder)
-                tar = tarfile.open("a.tar.gz", mode="w:gz")
-                tar.add(dirData + folder+"/", os.path.basename(dirData + folder + "/"))
+            pathName = dirData + folder
+            for fileName in os.listdir(pathName):
+                os.chdir(pathName)
+                fileNameBase = os.path.splitext(fileName)[0]
+                tar = tarfile.open(fileNameBase + ".tar.gz", mode="w:gz")
+                tar.add(pathName + "/" + fileName,
+                        arcname=fileName, recursive=False)
                 tar.close()
 
     def send_files(self):
         self.logger.info(cons.STARTING_SEND_FILES)
         self.compress_files()
-        # try:
-        #     response = self.client.put_object(
-        #         Body=(open(self.file_to_send+'.tar.gz', 'rb')),
-        #         Bucket='log-ec2-instance/log',
-        #         Key=self.file_to_send,
-        #     )
-        # except Exception as e:
-        #     self.logger.error(
-        #         "Something went wrong trying to send files: %s", e.__class__)
-        #     response = {'ResponseMetadata': {'HTTPStatusCode': 404}}
+        try:
+            response = self.client.put_object(
+                Body=(open(self.file_to_send+'.tar.gz', 'rb')),
+                Bucket='log-ec2-instance/log',
+                Key=self.file_to_send,
+            )
+        except Exception as e:
+            self.logger.error(
+                "Something went wrong trying to send files: %s", e.__class__)
+            response = {'ResponseMetadata': {'HTTPStatusCode': 404}}
 
-        # self.logger.info("Finishing data send to s3 bucket. Status: %s",
-        #                  response['ResponseMetadata']['HTTPStatusCode'])
+        self.logger.info("Finishing data send to s3 bucket. Status: %s",
+                         response['ResponseMetadata']['HTTPStatusCode'])
