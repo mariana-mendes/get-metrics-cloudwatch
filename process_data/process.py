5981fef7d45fc908c1cbbfef76d6eec5f103441e
Mariana Mendes <mariana.silva@ccc.ufcg.edu.br>
2020-09-07 22:57:21 -0300

    vo dormi

--- process_data/process.py
+++ process_data/process.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os.path
 from datetime import date
+from log.setup import setup_log
 
 
 def joinMetricsEC2(response, idInstance, metricName):
@@ -32,12 +33,13 @@ def joinMetricsEC2(response, idInstance, metricName):
     newDf = pd.DataFrame(data=newDict)
     today_file = date.today().strftime("%Y-%m-%d")
 
-    if(os.path.exists(today_file + '.csv')):
+    path = os.getcwd() + "/data/ec2/" + today_file + ".csv"
+    if(os.path.exists(path)):
         dtf = pd.read_csv(today_file + '.csv', index_col=0)
         newOne = dtf.append(newDf, ignore_index=True)
-        newOne.to_csv('/data/ec2', today_file + ".csv")
+        newOne.to_csv(path)
     else:
-        newDf.to_csv('/data/ec2', today_file + ".csv")
+        newDf.to_csv(path)
 
 
 def joinMetricsASG(response, groupName, metricName):
@@ -69,13 +71,13 @@ def joinMetricsASG(response, groupName, metricName):
     }
     newDf = pd.DataFrame(data=newDict)
     today_file = date.today().strftime("%Y-%m-%d")
-
+    path = os.getcwd() + "/data/asg/" + today_file + ".csv"
     try:
-    if(os.path.exists(today_file + '.csv')):
-        dtf = pd.read_csv(today_file + '.csv', index_col=0)
-        newOne = dtf.append(newDf, ignore_index=True)
-        newOne.to_csv('/data/asg', today_file + ".csv")
-    else:
-        newDf.to_csv('/data/asg', today_file + ".csv")
-    except Exception e:
+        if(os.path.exists(path)):
+            dtf = pd.read_csv(path, index_col=0)
+            newOne = dtf.append(newDf, ignore_index=True)
+            newOne.to_csv(path)
+        else:
+            newDf.to_csv(path)
+    except Exception as e:
         logger.error("erro ao criar arquivos", e.__class__)
