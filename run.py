23576d04cb384b45ae42a59638e8d70412c98c1d
mariana-mendes <mariana.silva@ccc.ufcg.edu.br>
2021-01-03 00:07:15 +0000

    Remove unused code

--- run.py
+++ run.py
@@ -13,11 +13,11 @@ import os
     Adjust the collector range and instantiate the Collector and the Sender'''
 with open(cons.CONFIG_FILE, 'r+') as f:
     data = json.load(f)
-    # endTime = datetime.utcnow().isoformat()
-    # startTime = (datetime.utcnow() -
-    #               timedelta(hours=cons.FREQUENCY_COLLECTOR)).isoformat()
-    # data[cons.END_TIME_KEY] = endTime
-    # data[cons.START_TIME_KEY] = startTime
+    endTime = datetime.utcnow().isoformat()
+    startTime = (datetime.utcnow() -
+                  timedelta(hours=cons.FREQUENCY_COLLECTOR)).isoformat()
+    data[cons.END_TIME_KEY] = endTime
+    data[cons.START_TIME_KEY] = startTime
     f.seek(0)
     json.dump(data, f, indent=4)
     f.truncate()
