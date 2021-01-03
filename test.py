0d0c79d95bb1b2fb1552e9fb2344d0d284653437
mariana-mendes <mariana.silva@ccc.ufcg.edu.br>
2021-01-03 21:30:39 +0000

    Cannot test with multiple instances

--- test.py
+++ test.py
@@ -14,12 +14,14 @@ from moto.core import ACCOUNT_ID
 import pandas as pd
 import numpy as np
 
+
+# pre-config
 utc_now = datetime.now(tz=pytz.utc)
 configJson = {
     "metricsDescription": [
         {
             "metricName": "metric",
-            "namespace": "tester",
+            "namespace": "AWS/Test",
             "dimension": "InstanceId",
             "statistics": ["SampleCount", "Sum"]
         }
@@ -27,24 +29,24 @@ configJson = {
     "endTime": (utc_now + timedelta(seconds=60)).isoformat(),
     "period": "60",
     "startTime": (utc_now - timedelta(seconds=60)).isoformat(),
-    "storage": {
-        "InstanceId": "teste-1"
-    },
+    "storage": {},
     "aws-config": {
         "region": "us-east-1",
-        "bucket": "jasjasjsaj"
     }
 }
 
+instanceMock = [{"InstanceId":"A"}]
+instancesMock = [{"InstanceId":"A"}, {"InstanceId":"B"}, {"InstanceId":"C"}]
 
+# create a collector instance
+collector = CWA(configJson[cons.METRICS_KEY],  configJson[cons.START_TIME_KEY],  configJson[cons.END_TIME_KEY], configJson[cons.PERIOD_KEY], configJson[cons.STORAGE], configJson[cons.AWS_CONFIG])
+
+# put the metrics you want to retrieve
 @mock_cloudwatch
-def test_get_metric_statistics():
-    conn = boto3.client("cloudwatch", region_name="us-east-1")
-    collector = CWA(configJson[cons.METRICS_KEY],  configJson[cons.START_TIME_KEY],  configJson[cons.END_TIME_KEY], configJson[cons.PERIOD_KEY], configJson[cons.STORAGE], configJson[cons.AWS_CONFIG])
+def put_metric_data(conn):
 
-    ## Inserindo métrica
     conn.put_metric_data(
-        Namespace="tester",
+        Namespace="AWS/Test",
         MetricData=[
             dict(
                 MetricName="metric",
@@ -55,10 +57,29 @@ def test_get_metric_statistics():
         ],
     )
 
+def put_metric_data_multiple_instances(conn, instances):
+    values = [1, 2, 4]
+    for i in range(len(instances)):
+        conn.put_metric_data(
+            Namespace="AWS/Test",
+            MetricData=[
+                dict(
+                    MetricName="metric",
+                    Dimensions=[{"Name": "InstanceId", "Value": instances[i]["InstanceId"]}],
+                    Value=values[i],
+                    Timestamp=utc_now,
+                )
+            ],
+        )
+    
 
-    ## Recuperando métrica
+@mock_cloudwatch
+def test_get_metric_statistics():
+    conn = boto3.client("cloudwatch", region_name="us-east-1")
+
+    put_metric_data(conn)
     stats = conn.get_metric_statistics(
-        Namespace="tester",
+        Namespace="AWS/Test",
         MetricName="metric",
         StartTime=utc_now - timedelta(seconds=60),
         EndTime=utc_now + timedelta(seconds=60),
@@ -66,23 +87,44 @@ def test_get_metric_statistics():
         Statistics=["SampleCount", "Sum"],
     )
 
-    all_metric_data = collector.retrieveFromCloudWatch(configJson[cons.METRICS_KEY][0], [{"InstanceId":"i-092381023"}])
+
+    all_metric_data = collector.retrieveFromCloudWatch(configJson[cons.METRICS_KEY][0], instanceMock)
     col = ['timestamp', 'InstanceId', 'metricName', 'SampleCount', 'Sum']
     newDf = pd.DataFrame(data=all_metric_data, columns = col)
 
-    SampleCount = newDf['SampleCount'][0]
-    Sum = newDf['Sum'][0]
-    length_of = len(all_metric_data)
-
     datapoint = stats["Datapoints"][0]
-
-    sumIsEqual = np.allclose(Sum,  datapoint["Sum"])
-    sampleCountIsEqual = np.allclose(SampleCount,  datapoint["SampleCount"])
+    sumIsEqual = np.allclose( newDf['Sum'][0],  datapoint["Sum"])
+    sampleCountIsEqual = np.allclose(newDf['SampleCount'][0],  datapoint["SampleCount"])
    
 
+    # Testing results
     sumIsEqual.should.be.true
     sampleCountIsEqual.should.be.true
-    stats["Datapoints"].should.have.length_of(length_of)
+    stats["Datapoints"].should.have.length_of(len(all_metric_data))
+
+
+@mock_cloudwatch
+def test_get_metric_statistics_multiple_instances():
+    conn = boto3.client("cloudwatch", region_name="us-east-1")
+    put_metric_data_multiple_instances(conn, instancesMock)
+
+    # TO-DO: Implement this test when this issue is done: 
+    # https://github.com/spulec/moto/blob/master/moto/cloudwatch/responses.py#L152
+
+#     stats = conn.get_metric_statistics(
+#                     Namespace="AWS/Test",
+#                     MetricName="metric",
+#                     Dimensions=[
+#                         {
+#                             "Name": "InstanceId",
+#                             "Value": "A"
+#                         },
+#                     ],
+#                     StartTime=utc_now - timedelta(seconds=60),
+#                     EndTime=utc_now + timedelta(seconds=60),
+#                     Period=60,
+#                     Statistics=["SampleCount", "Sum", "Maximum", "Minimum"],
+# )
 
 
 
